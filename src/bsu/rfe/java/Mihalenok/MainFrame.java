8b1c6a92767b932807978bd0ae795b818d123d20
phervufanika <120945224+phervufanika@users.noreply.github.com>
2023-05-26 03:35:15 +0300

    Initial commit

---
+++ src/bsu/rfe/java/Mihalenok/MainFrame.java
@@ -0,0 +1,90 @@
+package bsu.rfe.java.Mihalenok;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+    // Константы, задающие размер окна приложения, если оно
+// не распахнуто на весь экран
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+    private JButton slowDownButton = new JButton("-");
+    private JButton speedUpButton = new JButton("+");
+    // Поле, по которому прыгают мячи
+    private Field field = new Field();
+    // Конструктор главного окна приложения
+    public MainFrame() {
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+// Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+        setExtendedState(MAXIMIZED_BOTH);
+// Создать меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        slowDownButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                field.speedDown();
+            }
+        });
+        menuBar.add(slowDownButton);
+        speedUpButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                field.speedUp();
+            }
+        });
+        menuBar.add(speedUpButton);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+// Ни один из пунктов меню не являются
+// доступными - сделать доступным "Паузу"
+                    pauseMenuItem.setEnabled(true);
+                }
+            }
+        };
+
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+// Добавить в центр граничной компоновки поле Field
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
+    // Главный метод приложения
+    public static void main(String[] args) {
+// Создать и сделать видимым главное окно приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
